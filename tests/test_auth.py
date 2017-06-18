aadc018c52de48b1e05677c8b86daaf8db9b874f
r-wambui <rose.wambui@andela.com>
2017-06-18 17:42:53 +0300

    Added tearDown and setUp

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -2,28 +2,46 @@ import unittest
 import json
 import run
 
+from app.models import db
+
 
 class TestUserAuthentication(unittest.TestCase):
 
     def setUp(self):
         self.client = run.app.test_client()
-
-    def test_user_registration(self):
-        """test if a user is successfully created """
+        db.create_all()
         user = {"username": "rozzah", "password": "password"}
         userdata = json.dumps(user)
+        self.client.post(
+            "/v1/auth/register", data=userdata,
+            content_type="application/json")
+
         response = self.client.post(
-            "/v1/auth/register", data=userdata, content_type="application/json")
+            "/v1/auth/login", data=userdata, content_type="application/json")
+        self.token = json.loads(response.data.decode())['Authorization']
+
+    def test_register_user(self):
+        """test if a user is successfully created """
+        user = {"username": "rozzah1", "password": "password"}
+        userdata = json.dumps(user)
+        response = self.client.post(
+            "/v1/auth/register", data=userdata,
+            content_type="application/json")
         self.assertEqual(response.status_code, 201)
         output = json.loads(response.data)
         self.assertEqual(output['message'], "User created successfully")
 
-    def test_user_registration_user_already_exist(self):
+    def test_register_user_already_exist(self):
         """Test a user exist """
+
         user = {"username": "rozzah", "password": "password"}
         userdata = json.dumps(user)
+        self.client.post(
+            "/v1/auth/register", data=userdata,
+            content_type='application/json')
         response = self.client.post(
-            "/v1/auth/register", data=userdata, content_type='application/json')
+            "/v1/auth/register", data=userdata,
+            content_type='application/json')
         self.assertEqual(response.status_code, 409)
         output = json.loads(response.data.decode())
         self.assertEqual(output['error'], "User already exist")
@@ -37,7 +55,7 @@ class TestUserAuthentication(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         output = json.loads(response.data.decode())
         self.assertEqual(output['message'],
-                         "You have been loged in successfully")
+                         "You have been logged in successfully")
 
     def test_user_cannot_login_with_invalid_credentials(self):
         """Test a user cannot login in with invalid credentials"""
@@ -48,3 +66,6 @@ class TestUserAuthentication(unittest.TestCase):
         self.assertEqual(response.status_code, 401)
         output = json.loads(response.data.decode())
         self.assertEqual(output['error'], "Invalid username/password")
+
+    def tearDown(Self):
+        db.drop_all()
