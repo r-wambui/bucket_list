f24539559c7b63a1819ba53e4d172ec37ded28c6
r-wambui <rose.wambui@andela.com>
2017-06-18 17:36:25 +0300

    Edited the endpoints url

--- run.py
+++ run.py
@@ -1,22 +1,25 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
+
 
 from app.models import db
 from app.resources.auth import UserRegister, UserLogin
-from app.resources.bucketlist import BucketList, BucketLists, FetchBucketList, BucketListEdit 
+from app.resources.bucketlist import CreateBucketList, SingleBucketList, AllBucketList, BucketListEdit 
 from app.resources.item import BucketListItem,  EditBucketListItem
 
 
 
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 api.add_resource(UserRegister, '/v1/auth/register')
 api.add_resource(UserLogin, '/v1/auth/login')
-api.add_resource(FetchBucketList, '/v1/bucketlists/<int:id>')
-api.add_resource(BucketList, '/v1/bucketlist')
-api.add_resource(BucketLists,'/v1/bucketlists ')
+api.add_resource(SingleBucketList, '/v1/bucketlists/<int:id>')
+api.add_resource(CreateBucketList, '/v1/bucketlists')
+api.add_resource(AllBucketList,'/v1/bucketlists')
 api.add_resource(BucketListEdit, '/v1/bucketlists/<int:id>')
 api.add_resource(BucketListItem, '/v1/bucketlists/<int:id>/item')
 api.add_resource(EditBucketListItem, '/v1/bucketlists/<int:bucket_id>/item/<int:item_id>')
